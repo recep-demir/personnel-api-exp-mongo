66a1b19a0b3e085d02e0504d56b62d8f61bb85c9
recep-demir <demir.rp@gmail.com>
2025-04-20 23:27:26 +0200

    refactor: Iimprove code structure in department.js

--- src/routes/department.js
+++ src/routes/department.js
@@ -1,11 +1,16 @@
 "use strict"
-/* -------------------------------------------------------
-    EXPRESS - Personnel API
-------------------------------------------------------- */
+
 const router = require('express').Router()
-/* ------------------------------------------------------- */
+const department = require('../controllers/department')
+
+router.route('/').get(department.list).post(department.create)
+
+router.route("/:id")
+.get(department.read)
+.put(department.update)
+.patch(department.update)
+.delete(department.delete)
 
 
 
-/* ------------------------------------------------------- */
 module.exports = router
