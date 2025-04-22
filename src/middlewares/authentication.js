3f8f4c81c3a22eacd4995d986143c9133508170b
recep-demir <demir.rp@gmail.com>
2025-04-22 18:18:10 +0200

    feat: add authentication middleware to handle token validation and user retrieval

---
+++ src/middlewares/authentication.js
@@ -0,0 +1,26 @@
+"use strict"
+
+const Token = require('../models/token');
+
+module.exports = async (req, res, next) => {
+
+    req.user = null;
+
+    const auth = req.headers?.authorization || null;
+
+    const tokenKey = auth ? auth.split(' ') : null
+    //console.log(tokenKey[1])
+
+    if (tokenKey && tokenKey[0]=='Token') {
+        const tokenData = await Token.findOne({ token: tokenKey[1] }).populate('userId');
+        if (tokenData) req.user = tokenData.userId
+    }
+
+   
+    
+
+    
+
+
+next()
+}
