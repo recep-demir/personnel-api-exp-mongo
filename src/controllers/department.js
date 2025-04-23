88452cc26320dc517cfcfaff838d35b104110acc
recep-demir <demir.rp@gmail.com>
2025-04-23 21:27:20 +0200

    refactor: improve code formatting and consistency in department controller

--- src/controllers/department.js
+++ src/controllers/department.js
@@ -1,59 +1,81 @@
 "use strict"
 
-const Department=require("../models/department")
+const Department = require("../models/department");
+const Personnel = require('../models/personnel');
 
 module.exports = {
-    list: async (req,res)=> {
+    list: async (req, res) => {
+
         const result = await res.getModelList(Department)
 
         res.status(200).send({
-            error:false,
-            details:await result.getModelListDetails(Department),
+            error: false,
+            details: await res.getModelListDetails(Department),
             result
-        })      
-    },
-    create:async (req,res)=>{
+        })
 
-        const result=await Department.create(req.body)
+    },
+
+    create: async (req, res) => {
+
+        const result = await Department.create(req.body)
 
         res.status(201).send({
-            error:false,
+            error: false,
             result
         })
 
     },
-    read:async (req,res)=>{
+    read: async (req, res) => {
 
-        const result=await Department.findOne({ _id:req.params.id}) 
-        //Exp: /departments/661f... → req.params.id = "661f..."
+        const result = await Department.findOne({ _id: req.params.id })
 
         res.status(200).send({
-            error:false,
+            error: false,
             result
         })
 
     },
+    update: async (req, res) => {
 
-    update:async (req,res)=>{
-
-        const result=await Department.updateOne({ _id:req.params.id},req.body,{
-            runValidators:true,  // runs validation methods
-            new:true}) // return updated data
+        const result = await Department.updateOne({ _id: req.params.id }, req.body, {
+            runValidators: true,  // runs validation methods
+            new: true
+        }) // return updated data
 
         res.status(202).send({
-            error:false,
+            error: false,
             result
         })
 
     },
-    delete:async (req,res)=>{
+    delete: async (req, res) => {
 
-        const result=await Department.deleteOne({ _id:req.params.id})
+        const result = await Department.deleteOne({ _id: req.params.id })
 
-            res.status(result.deletedCount ? 204 : 404).send({
-            error:true,
-            message:"Data is not found  or deleted"
+        // 204 no content - 404 : not found
+        res.status(result.deletedCount ? 204 : 404).send({
+            error: true,
+            message: "Data is not found  or deleted"
         })
 
+    },
+
+    // todo : ilgili departmandaki tum kisileri listeleme
+
+    personnels: async (req, res) => {
+
+        const { id: departmentId } = req.params;
+
+        const result = await res.getModelList(Personnel, { departmentId });
+
+        res.status(200).send({
+            error: false,
+            details: await res.getModelListDetails(Personnel, { departmentId }),
+            result
+        })
     }
+
+
+
 }
