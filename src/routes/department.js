e47b819604caa47bed3adfdd03cef324b4dda138
recep-demir <demir.rp@gmail.com>
2025-04-23 21:15:04 +0200

    feat: add authentication checks for department routes

--- src/routes/department.js
+++ src/routes/department.js
@@ -1,16 +1,15 @@
 "use strict"
 
 const router = require('express').Router()
-const department = require('../controllers/department')
+const department = require("../controllers/department")
+const { isLogin, isAdmin, isAdminOrLead } = require('../middlewares/permissions');
 
-router.route('/').get(department.list).post(department.create)
+router.route("/").get(isLogin, department.list).post(isAdmin, department.create)
 
 router.route("/:id")
-.get(department.read)
-.put(department.update)
-.patch(department.update)
-.delete(department.delete)
+    .get(isLogin, department.read)
+    .put(isAdminOrLead, department.update)
+    .patch(isAdminOrLead, department.update)
+    .delete(isAdmin, department.delete);
 
-
-
-module.exports = router
+router.get('/:id/personnels', department.personnels)
