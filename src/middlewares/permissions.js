aabcbc5c3e9197785c24d5b8448d87c91b365ab7
recep-demir <demir.rp@gmail.com>
2025-04-23 21:12:33 +0200

    feat: add isAdminOrLead middleware for department lead permissions

--- src/middlewares/permissions.js
+++ src/middlewares/permissions.js
@@ -22,5 +22,18 @@ module.exports = {
         }
 
     },
+    isAdminOrLead: (req,res,next) => {
+        const departmentId = req.params?.id
+        
+        if (req.user && req.user.isActive && (req.user.isAdmin || req.user.isLead)) {
+            // if (req.user && req.user.isActive && (req.user.isAdmin || (req.user.isLead && req.user.departmentId == departmentId))) {
+    
+                next()
+    
+            } else {
+                res.errorStatusCode = 403
+                throw new Error('NoPermission: You must login and to be Admin or Department Lead (Own).')
+            }
+    }
 
 }
