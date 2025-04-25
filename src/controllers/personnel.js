8ea5d5852f8c3fd7c671c5dec2d1bc4d504c5b23
recep-demir <demir.rp@gmail.com>
2025-04-25 17:53:34 +0200

    feat: add Swagger documentation for Personnel CRUD operations

--- src/controllers/personnel.js
+++ src/controllers/personnel.js
@@ -5,6 +5,21 @@ const Personnel = require("../models/personnel")
 module.exports = {
     list: async (req, res) => {
 
+        /* 
+            #swagger.tags = ['Personnels']
+            #swagger.summary = 'List Personnels'
+            #swagger.description = `
+                You can send query with endpoint for search[], sort[], page and limit.
+                <ul> Examples:
+                    <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
+                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                    <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
+                    <li>URL/?<b>page=2&limit=1</b></li>
+                </ul>
+            `
+            }
+        */
+
         const result = await res.getModelList(Personnel)
 
         res.status(200).send({
@@ -16,6 +31,18 @@ module.exports = {
     },
 
     create: async (req, res) => {
+
+        /*
+            #swagger.tags = ["Personnels"]
+            #swagger.summary = "Create Personnel"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    $ref: '#/definitions/Personnel'
+                }
+            }
+        */
 
         const isLead = req.body?.islead || false
 
@@ -33,6 +60,11 @@ module.exports = {
     },
     read: async (req, res) => {
 
+        /*
+            #swagger.tags = ["Personnels"]
+            #swagger.summary = "Get Single Personnel"
+        */
+
         const result = await Personnel.findOne({ _id: req.params.id })
 
         res.status(200).send({
@@ -42,6 +74,18 @@ module.exports = {
 
     },
     update: async (req, res) => {
+
+        /*
+            #swagger.tags = ["Personnels"]
+            #swagger.summary = "Update Personnel"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    $ref: '#/definitions/Personnel'
+                }
+            }
+        */
 
         const isLead = req.body?.islead || false
 
@@ -63,6 +107,11 @@ module.exports = {
 
     },
     delete: async (req, res) => {
+
+        /*
+            #swagger.tags = ["Personnels"]
+            #swagger.summary = "Delete Personnel"
+        */
 
         const result = await Personnel.deleteOne({ _id: req.params.id })
 
