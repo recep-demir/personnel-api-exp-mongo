ad2f22b29d8ea09e5322f2c09e91b3e920e5f73a
recep-demir <demir.rp@gmail.com>
2025-04-25 17:53:30 +0200

    feat: enhance Swagger documentation for department endpoints

--- src/controllers/department.js
+++ src/controllers/department.js
@@ -6,6 +6,21 @@ const Personnel = require('../models/personnel');
 module.exports = {
     list: async (req, res) => {
 
+        /* 
+            #swagger.tags = ['Departments']
+            #swagger.summary = 'List Departments'
+            #swagger.description = `
+                You can send query with endpoint for search[], sort[], page and limit.
+                <ul> Examples:
+                    <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
+                    <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                    <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
+                    <li>URL/?<b>page=2&limit=1</b></li>
+                </ul>
+            `
+            }
+        */
+
         const result = await res.getModelList(Department)
 
         res.status(200).send({
@@ -18,6 +33,18 @@ module.exports = {
 
     create: async (req, res) => {
 
+        /*
+            #swagger.tags = ["Departments"]
+            #swagger.summary = "Create Department"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    $ref: '#/definitions/Department'
+                }
+            }
+        */
+
         const result = await Department.create(req.body)
 
         res.status(201).send({
@@ -28,6 +55,11 @@ module.exports = {
     },
     read: async (req, res) => {
 
+          /*
+            #swagger.tags = ["Departments"]
+            #swagger.summary = "Get Single Department"
+        */
+
         const result = await Department.findOne({ _id: req.params.id })
 
         res.status(200).send({
@@ -37,6 +69,18 @@ module.exports = {
 
     },
     update: async (req, res) => {
+
+        /*
+            #swagger.tags = ["Departments"]
+            #swagger.summary = "Update Department"
+            #swagger.parameters['body'] = {
+                in: 'body',
+                required: true,
+                schema: {
+                    $ref: '#/definitions/Department'
+                }
+            }
+        */
 
         const result = await Department.updateOne({ _id: req.params.id }, req.body, {
             runValidators: true,  // runs validation methods
@@ -51,6 +95,12 @@ module.exports = {
     },
     delete: async (req, res) => {
 
+         /*
+            #swagger.tags = ["Departments"]
+            #swagger.summary = "Delete Department"
+        */
+
+
         const result = await Department.deleteOne({ _id: req.params.id })
 
         // 204 no content - 404 : not found
@@ -62,6 +112,21 @@ module.exports = {
     },
 
     personnels: async (req, res) => {
+
+        /* 
+                #swagger.tags = ['Departments']
+                #swagger.summary = 'List Personnels of Department'
+                #swagger.description = `
+                    You can send query with endpoint for search[], sort[], page and limit.
+                    <ul> Examples:
+                        <li>URL/?<b>filter[field1]=value1&filter[field2]=value2</b></li>
+                        <li>URL/?<b>search[field1]=value1&search[field2]=value2</b></li>
+                        <li>URL/?<b>sort[field1]=1&sort[field2]=-1</b></li>
+                        <li>URL/?<b>page=2&limit=1</b></li>
+                    </ul>
+                `
+                }
+            */
 
         const { id: departmentId } = req.params;
 
